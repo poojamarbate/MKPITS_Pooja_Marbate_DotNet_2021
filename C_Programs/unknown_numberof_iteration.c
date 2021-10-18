78cb26dc04d1452bb38fafb5b0666dbfec152a5d
Pooja Marbate <poojamarbate090@gnail.com>
2021-10-18 11:46:20 +0530

    C Program

--- C_Programs/unknown_numberof_iteration.c
+++ C_Programs/unknown_numberof_iteration.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ double number;
+ printf("enter number:");
+ scanf("%lf",&number);
+ 
+ 	while(number>=0.0)
+	{ 
+	 printf("your entered %.2lf\n",number);
+	 
+	 printf("enter a number:");
+	 scanf("%lf",&number);
+	 
+	}
+	
+	return 0;
+}	 
+	
 
