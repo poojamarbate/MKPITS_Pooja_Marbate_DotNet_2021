195125caad300c030b5fa29422e2d04be0296b3e
Pooja Marbate <poojamarbate090@gnail.com>
2021-10-19 02:25:03 +0530

    deleted files

--- C_Programs/Assignment_operator.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
- int a=100,b=200;
- a=b;
- printf("a=%d",a);
- return 0;
- }
-
--- C_Programs/Type_Conversion1.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
- double n1=5.4,sum;
- sum=5+n1;
- printf("%lf",sum);
- return 0;
- }
-
