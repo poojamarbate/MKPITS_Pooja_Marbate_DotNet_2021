f29965cc240173dc086b2bac5328c1e3258b6e3a
Pooja Marbate <poojamarbate090@gnail.com>
2021-11-10 17:30:08 +0530

    Assignment operator

--- HTML_CSS_Javascript/Assignment_Programs/H_Nov9_AssigmentOperator.js
+++ HTML_CSS_Javascript/Assignment_Programs/H_Nov9_AssigmentOperator.js
@@ -1,5 +1,13 @@
 var x=10;
 var y=5;
-X+=x+y;
-document.write("X+ ="+X);
-console.log(X);
+x+=y;
+//x+=y      x=x+y
+document.write("increment Assignment operator = "+x);
+document.write("</br>")
+console.log(x);
+
+var x=10;
+var y=5;
+x-=y;
+document.write("decrement assignment operator  = "+x);
+console.log(x);
