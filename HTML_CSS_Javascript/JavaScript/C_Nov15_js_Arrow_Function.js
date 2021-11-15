412f48c1a8d03f86e85202b6b9b589f4e6b382b6
Pooja Marbate <poojamarbate090@gnail.com>
2021-11-15 12:27:12 +0530

    class program

---
+++ HTML_CSS_Javascript/JavaScript/C_Nov15_js_Arrow_Function.js
@@ -0,0 +1,12 @@
+var func = (a,b) =>{
+    var sum = a + b;
+    return sum;
+}
+console.log("sum of 1 and 3: ",func(1,3));
+
+function func(a,b)
+{
+    var sum = a + b;
+    return sum ;
+}
+console.log("sum of 1 and 3 : "+func(1,3));
---
+++ HTML_CSS_Javascript/JavaScript/C_Nov15_js_arrow_function_sub_multi.js
@@ -0,0 +1,11 @@
+var func = (a,b) =>{
+    var multi = a * b ;
+    return multi;
+}
+console.log(" Multiplication of 4 and 2 : ",func(4,2));
+
+var func = (a,b) =>{
+    var sub = a - b;
+    return sub;
+}
+console.log(" Subtraction of 4 to 2 : ",func(4,2));
