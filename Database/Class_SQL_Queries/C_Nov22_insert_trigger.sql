a1a72de928d600bedeefad369eb5bab67abd3ba6
Pooja Marbate <poojamarbate090@gnail.com>
2021-11-23 11:47:25 +0530

    SQL

---
+++ Database/Class_SQL_Queries/C_Nov22_insert_trigger.sql
@@ -0,0 +1,2 @@
+insert into employee_test values('veer',34000);
+select * from employee_Test_audit
--- Database/Class_SQL_Queries/C_Nov22_sales.sql
+++ Database/Class_SQL_Queries/C_Nov22_sales.sql
@@ -3,7 +3,7 @@ create table sales (id int identity(1,1),itemname varchar(20),price decimal(12,0
  insert into sales(itemname,price,qty)values('RedMe 10S',500,10);
  insert into sales(itemname,price,qty)values('Samsung Laptop',100,20);
  insert into sales(itemname,price,qty)values('Canon Photo Coppier',300,2);
- --------------------------------
+ ----------------------------------------------------------------------------------
  begin 
  declare @sales int;
  select @sales=sum(price*qty)from sales;----select sum(price*qty)from sales
