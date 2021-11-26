ce4deb1cc3f290e59d0600d739243ddb6d1b8948
Pooja Marbate <poojamarbate090@gnail.com>
2021-11-26 12:03:36 +0530

    c sharp

---
+++ C_sharp/C_Nov21_MyConsoleApp1/C_Nov26_Class_Object_Example/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Nov26_Class_Object_Example
+{
+    class Employee
+    {//private,public,protected,internal
+       private int id;
+       public string firstname, lastname;
+        public decimal salary;
+    }
+}
---
+++ C_sharp/C_Nov21_MyConsoleApp1/C_Nov26_Class_Object_Example/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace C_Nov26_Class_Object_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Employee obj, obj1;
+            //int x, y;//primitive data type have single value
+            //x = 10;
+            //y = 50;
+            //obj = new Employee();//user defined data type
+            //obj.firstname = "Pooja";
+            //obj.lastname = "Marbate";
+            //obj.salary = 35000;
+
+            //Console.WriteLine(obj.firstname+" "+obj.lastname+" "+obj.salary);
+            //obj1 = new Employee();
+            //obj1.firstname = "Ram";
+            //obj1.lastname = "Kapoor";
+            //obj1.salary = 40000;
+
+            //Date birthday = new Date(); //constructor
+            ////birthday.day = 5;
+            ////birthday.month = 6;
+            ////birthday.year = 1998;
+            //Console.WriteLine("Pooja's Birthday is "+birthday.day+"/"+birthday.month+"/"+birthday.year);
+
+            ////constructor Example
+
+            Date today = new Date(26, 11, 2021);
+            today.Display();
+            Date birthday = new Date(5, 6, 1998);
+            Console.WriteLine("Pooja's Birthday is : ");
+            birthday.Display();
+            Date mybirthday = new Date(26, 8, 80);
+            // Console.WriteLine(mybirthday.GetType());
+            mybirthday.Display();
+
+
+                
+        }
+    }
+}
