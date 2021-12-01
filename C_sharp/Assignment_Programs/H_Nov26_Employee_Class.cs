764ee1b7d55c28123a98527fa6c1a45f6594f851
Pooja Marbate <poojamarbate090@gnail.com>
2021-12-01 10:57:06 +0530

    C sharp

--- C_sharp/Assignment_Programs/H_Nov26_Employee_Class.cs
+++ C_sharp/Assignment_Programs/H_Nov26_Employee_Class.cs
@@ -37,5 +37,15 @@ namespace H_Nov29_Employee_Class_Example
         {
             Console.WriteLine("Totol Employees : "+count);
         }
+        public decimal GrossSalary()
+        {
+            decimal Gross = salary + salary * 0.75m + salary * 0.1m + 1500;
+            return Gross;
+        }
+        public decimal NetSalary()
+        {
+            decimal Net = GrossSalary() - 768.7m;
+            return Net;
+        }
     }
 }
