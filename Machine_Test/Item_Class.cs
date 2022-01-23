9263dfa7169eb857e8b14b187313de364206899a
Pooja Marbate <poojamarbate090@gnail.com>
2022-01-23 21:44:22 +0530

    Collection

---
+++ Machine_Test/Item_Class.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collection
+{
+    class Item
+    {
+        public int ItemId { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+
+       
+        public Item(int ID ,decimal Price, string Name)
+        {
+            this.Price = Price;
+            this.ItemId = ID;
+            this.Name = Name;
+        }
+
+        public override string ToString()
+        {
+            return ItemId+" ,"+Name+" ,"+Price;
+        }
+        public decimal GetAmount(int Qty)
+        {
+            return Price * Qty;
+        }
+        
+    }
+}
---
+++ Machine_Test/Project_Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Collection
+{
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Item> Items = new List<Item>();
+            List<decimal> Amounts = new List<decimal>();
+     
+            Console.WriteLine("How many items you want to purchase : ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter ItemID: ");
+                int ID = Convert.ToInt32(Console.ReadLine());
+
+
+                Console.WriteLine("Enter Product Name : ");
+                string product = Console.ReadLine();
+
+                Console.WriteLine("Enter Price : ");
+                decimal Price = Convert.ToDecimal(Console.ReadLine());
+
+                Console.WriteLine("Enter Qty : ");
+                int Qty = Convert.ToInt32(Console.ReadLine());
+               
+                Item I= new Item(ID, Price, product);
+
+                Items.Add(I);
+
+                Amounts.Add(I.GetAmount(Qty));
+            }
+
+            foreach (object obj in Items)
+            {
+                Console.WriteLine(obj);
+            }
+            Console.WriteLine("");
+            Console.WriteLine("Amounts are as follows :");
+            foreach (decimal amt in Amounts)
+            {
+                Console.WriteLine(amt);
+            }
+
+            Console.ReadKey();
+        }
+
+        
+    }
+}
