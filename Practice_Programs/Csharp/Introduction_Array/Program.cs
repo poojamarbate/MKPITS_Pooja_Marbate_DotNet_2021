730e11be30b4325e446dbfec2561f7eafc3e3b28
Pooja Marbate <poojamarbate090@gnail.com>
2022-02-11 13:59:40 +0530

    Array and foreach loop

---
+++ Practice_Programs/Csharp/Introduction_Array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Introduction_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Array: Collection of elements of a single data type stored in 
+             * adjacent memeory location.
+             * 
+             * elements are ccessed using index numbers
+             *  -ve index is not possible.
+             */
+
+
+
+            //int[] myarray = new int[3];
+            //myarray[0] = 23;
+            //myarray[1] = 56;
+            //myarray[2] = 78;
+
+            //string[] myarry = new string[] { "Pooja", "Jay", "Ishu", "Suju", "suriya" };
+
+            string[] myarry = { "Pooja", "Jay", "Ishu", "Suju", "suriya" };
+            //Console.WriteLine(myarry[0]);
+            Console.WriteLine(myarry.Length);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Practice_Programs/Csharp/Using_Foreach_Loop/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Using_Foreach_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Foreach Loop:C# is an extension of the for loop
+             */
+            int[] myarry = new int[5];
+            myarry[0] = 10;
+            myarry[1] = 20;
+            myarry[2] = 30;
+            myarry[3] = 40;
+            myarry[4] = 50;
+            
+
+            for (int i = 0; i < myarry.Length; i++)//length actual size of array
+            {
+                Console.WriteLine(myarry[i]);//counter variable
+            }
+
+            Console.WriteLine("---------------Foreach loop--------------");
+            //foreach loop for retrive array elemts
+            //advance version of for loop
+            //specifiy data type
+            //particularly useful for reference type data
+            foreach (int item in myarry)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
