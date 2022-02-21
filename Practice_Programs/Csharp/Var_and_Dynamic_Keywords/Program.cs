152e15341622cc4712882ede05d042177a434541
Pooja Marbate <poojamarbate090@gnail.com>
2022-02-21 16:08:34 +0530

    Var and Dynamic Keyword

---
+++ Practice_Programs/Csharp/Var_and_Dynamic_Keywords/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Var_and_Dynamic_Keywords
+{
+    class Program
+    {
+        //public static int Show(var b,int a)
+        //{
+        //    return 0;
+        //}
+
+        public static dynamic Show(dynamic a)
+        {
+            return a;
+        }
+        static void Main(string[] args)
+        {
+
+            //int a = 20;//only store integer value
+            //var b = "Pooja";//it store all type of data.
+            ////var keyword is value type
+            //Console.WriteLine(b.GetType());//to find the data type use get.method()
+            //Console.WriteLine(b.Length);
+            //Console.WriteLine(b.ToUpper());
+
+            //Dynamic 
+
+           // dynamic a = "Pooja";
+          
+            dynamic b;
+            b = 10;
+            //Console.WriteLine(a.GetType());
+            Program.Show("pooja");
+            Program.Show(5);
+            Program.Show(true);
+            Console.ReadLine();
+        }
+    }
+}
