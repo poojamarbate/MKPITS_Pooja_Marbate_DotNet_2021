ac285afe06cb74ef7ba3d3c968cd2cc29e1a3f22
Pooja Marbate <poojamarbate090@gnail.com>
2022-02-21 18:04:17 +0530

    Constructor

---
+++ Practice_Programs/Csharp/Constructor_In_Csharp/Employees.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_In_Csharp
+{
+    class Employees
+    {
+        int Emp_Id;
+        string Emp_Name;
+        int Emp_Age;
+        public Employees(int id,string name,int age)
+        {
+            this.Emp_Id = id;
+            this.Emp_Name = name;
+            this.Emp_Age = age;
+        }
+
+        //getter method
+        public int getID()
+        {
+            return this.Emp_Id;
+        }
+        public string getName()
+        {
+            return this.Emp_Name;
+        }
+        public int getAge()
+        {
+            return this.Emp_Age;
+        }
+    }
+}
---
+++ Practice_Programs/Csharp/Constructor_In_Csharp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_In_Csharp
+{
+    class Program
+    {
+        //no need to call constructor.
+        //default constructor.
+        public Program()
+        {
+            Console.WriteLine("Constructor invokes !!!");
+        }
+
+        //parameterized constructor
+        
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            Employees e = new Employees(1, "Jayesh Kumar", 34);
+            Console.WriteLine("Employee Id : {0}",e.getID());
+            Console.WriteLine("Employee Name : {0}", e.getName());
+            Console.WriteLine("Employee  Age : {0}", e.getAge());
+
+            Console.ReadLine();
+        }
+    }
+}
