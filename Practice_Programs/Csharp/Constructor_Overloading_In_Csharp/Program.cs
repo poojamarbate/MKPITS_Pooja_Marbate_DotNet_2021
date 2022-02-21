d559ad5a80555f4b2bf0bf235cc769e1299767aa
Pooja Marbate <poojamarbate090@gnail.com>
2022-02-21 18:05:22 +0530

    Constructor overloading

---
+++ Practice_Programs/Csharp/Constructor_Overloading_In_Csharp/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_Overloading_In_Csharp
+{
+    class Program
+    {
+        //same name but different signature. 
+        public Program()
+        {
+            Console.WriteLine("This is a default constructor....");
+        }
+        public Program(int a,int b)
+        {
+            Console.WriteLine("This is a second constructor....{0}",(a+b));
+        }
+
+        public Program(int a, int b,int c)
+        {
+            Console.WriteLine("This is a third  constructor....{0}", (a + b+c));
+        }
+
+
+        static void Main(string[] args)
+        {
+            //constructor overloading
+            //decide in complie time which constructor is call.
+            Program p = new Program();
+            Program e= new Program(20,30);
+            Program q = new Program(20,30,40);
+            Console.ReadLine();
+        }
+    }
+}
